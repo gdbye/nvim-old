b0664b9beec476a6784abc56668b6470561d43cf
varbut <basens21@gmail.com>
2024-05-28 23:07:35 +0300

    modified:   lazy-lock.json
    	modified:   lua/plugins.lua
    	modified:   lua/telescope.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -64,5 +64,15 @@ require("lazy").setup({
 		}
 	},
 	{'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons'},
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		}
+	},
+
 
 })
--- lua/telescope.lua
+++ lua/telescope.lua
@@ -1,5 +1,21 @@
+-- Require necessary plugins
+local wk = require("which-key")
 local builtin = require('telescope.builtin')
+
+-- Define your key mappings and their descriptions
+wk.register({
+  f = {
+    name = "Telescope", -- Group name for 'f' prefix
+    f = { builtin.find_files, "Find Files" },
+    g = { builtin.live_grep, "Live Grep" },
+    b = { builtin.buffers, "Buffers" },
+    h = { builtin.help_tags, "Help Tags" },
+  },
+}, { prefix = "<leader>" })
+
+-- You can also set up the mappings directly using vim.keymap.set, but with descriptions via which-key
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
